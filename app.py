0edcc98883454a17370031a2a491770aa7c95b34
takingnote005 <carey816@gmail.com>
2022-05-25 19:05:25 +0800

    check-point 3

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h2>Hello World</h2>'
+
+if __name__=='__main__':
+    app.run("127.0.0.1",5050,debug=True)
